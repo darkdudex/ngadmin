3da3de014c3439361f8fb5dc11e83c8e1b347244
darkdudex <darkdudex@gmail.com>
2017-08-14 10:31:37 +0300

    ismail

---
+++ app/scripts/controllers/shop-single-po.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name minovateApp.controller:ShopSingleOrderCtrl
+ * @description
+ * # ShopSingleOrderCtrl
+ * Controller of the minovateApp
+ */
+app
+  .controller('SinglePoCtrl', function ($scope) {
+    $scope.page = {
+      title: 'Purchase Order',
+      subtitle: 'Place subtitle here...'
+    };
+  });
